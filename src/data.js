5cb1c45d8c7b07d818d274da2c5df2ec43b4cfec
Oluwafemi Mathias A <36882040+codydematth@users.noreply.github.com>
2023-01-12 06:40:28 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -42,7 +42,7 @@ const projectList = [
     backgroundImage:
       'https://img.freepik.com/free-vector/dark-gradient-background-with-copy-space_53876-99548.jpg?size=626&ext=jpg&ga=GA1.2.2102900112.1628985600',
     frontImage: 'https://codydematth.pythonanywhere.com/media/img/tekol.png',
-    liveLink: 'http://www.tekolafrica.com/',
+    liveLink: 'https://tekolafrica.up.railway.app/',
     githubLink: 'https://github.com/codydematth',
   },
   {
